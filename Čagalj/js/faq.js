e432a3b6fc6d8b391c6d29e418cc173e420e89a0
deniskulusic <109351503+deniskulusic@users.noreply.github.com>
2023-03-21 07:22:47 +0100

    Add files via upload

--- Čagalj/js/faq.js
+++ Čagalj/js/faq.js
@@ -1,5 +1,10 @@
-const button = document.querySelector(".checkbox");
-const buttonPressed = (e) => {
-  e.target.classList.toggle("active");
+const acordation=document.getElementsByClassName('faq');
+for(i=0;i<acordation.length;i++){
+    acordation[i].addEventListener('click',function(){
+        var elems = document.querySelectorAll(".faq.active");
+[].forEach.call(elems, function(el) {
+    el.classList.remove("active");
+});
+        this.classList.toggle('active');
+    })
 }
-button.addEventListener("click", buttonPressed);
