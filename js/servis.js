22912f75305aeda99765310c7dba66c90466c136
deniskulusic <109351503+deniskulusic@users.noreply.github.com>
2023-04-13 18:13:25 +0200

    Update servis.js

--- js/servis.js
+++ js/servis.js
@@ -19,6 +19,9 @@ function scroll() {
     
     paralax.style.position="relative"
   }
+  else{
+    paralax.style.position="sticky"
+  }
     if(wrap.scrollTop>(darkfromtop + darkheight)){
         body.classList.remove("inverted");
     }
